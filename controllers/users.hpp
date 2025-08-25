9a7214bae1fe511d600898acfc4170294ce46270
uzo <uzoochogu@yahoo.com>
2025-08-25 22:57:08 +0100

    [chore] - code cleanup, bug fixes, used static func in search and users

--- controllers/users.hpp
+++ controllers/users.hpp
@@ -2,20 +2,19 @@
 
 #include <drogon/HttpController.h>
 
-using namespace drogon;
-
 namespace api {
 namespace v1 {
 
 class Users : public drogon::HttpController<Users> {
  public:
   METHOD_LIST_BEGIN
-  ADD_METHOD_TO(Users::get_users, "/api/v1/users", Get, Options,
+  ADD_METHOD_TO(Users::get_users, "/api/v1/users", drogon::Get, drogon::Options,
                 "CorsMiddleware", "AuthMiddleware");
   METHOD_LIST_END
 
-  Task<> get_users(HttpRequestPtr req,
-                   std::function<void(const HttpResponsePtr&)> callback);
+  static drogon::Task<> get_users(
+      drogon::HttpRequestPtr req,
+      std::function<void(const drogon::HttpResponsePtr&)> callback);
 };
 
 }  // namespace v1
